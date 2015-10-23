77450e3af75b34a922c7d3258be15bb5d409919f
Jake Deichert <jakedeichert@users.noreply.github.com>
2015-10-22 20:42:17 -0400

    Rename index.md to index.html and add nav component
    Rovr supports both md and html files although html files don’t get ran
    through `marked`.

---
+++ _components/Nav.jsx
@@ -0,0 +1,13 @@
+/**
+ * Nav Component
+ * @desc Renders the top navigation bar.
+ */
+
+class Nav extends React.Component {
+    render() {
+        return <nav className="nav">
+            <a href="/docs">Docs</a>
+            <a href="/api">API</a>
+        </nav>;
+    }
+}
