00e0a94e2f4b129626d88c50d7d06edc5a06b059
Jake Deichert <jakedeichert@users.noreply.github.com>
2016-01-11 22:49:58 -0500

    Adjust urls for github pages

--- _components/Nav.jsx
+++ _components/Nav.jsx
@@ -6,9 +6,9 @@
 let Nav = (props) => {
     return <nav className="nav">
         <div className="pages">
-            <a href="/">Home</a>
-            <a href="/docs">Docs</a>
-            <a href="/api">API</a>
+            <a href="/rovr-website">Home</a>
+            <a href="/rovr-website/docs">Docs</a>
+            <a href="/rovr-website/api">API</a>
         </div>
         <div className="social">
             <a href="https://github.com/jakedeichert/rovr"><GitHubIcon width="25" height="25" color="#fff"/></a>
